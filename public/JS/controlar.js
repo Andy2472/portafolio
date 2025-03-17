9f2a788da6e32e6aa33d1b158d2fd9f1f13b2afd
Andy2472 <89529008+Andy2472@users.noreply.github.com>
2025-03-17 08:21:11 -0500

    Add files via upload

---
+++ public/JS/controlar.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#write_my_name', {
+    strings: ['IT\'S YILMAR ANDRES RODRIGUEZ LASSO.', ' SOFTWARE ENGINEER.'],
+    typeSpeed:70,
+    backSpeed: 60,
+    backDelay: 1000,
+    startDelay: 0,
+    loop: true
+  });
